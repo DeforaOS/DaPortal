5589115913c7c2aafe423a31711909059a5c5fc6
Pierre Pronchery <khorben@defora.org>
2013-03-12 13:30:49 +0100

    Added a debugging mode

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2012 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org>
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, version 3 of the License.
@@ -16,9 +16,11 @@
 
 
 #variables
+DEBUG=
 PACKAGE=
 VERSION=
 PREFIX="/usr/local"
+VERBOSE=0
 [ -f "./config.sh" ] && . "./config.sh"
 
 CP="cp -f"
@@ -32,10 +34,51 @@ TMPDIR="/tmp"
 
 
 #functions
+_deploy()
+{
+	DAPORTALDIR="$1"
+	REMOTE="$2"
+	TARX="$TAR -xzf"
+
+	[ "$VERBOSE" -ne 0 ] && TARX="$TAR -xzvf"
+	$DEBUG $MAKE dist					|| return 2
+	$DEBUG $SCP -- "$ARCHIVE" "$REMOTE:$TMPDIR"		|| return 2
+	$DEBUG $SSH "$REMOTE" "sh -c \"$MKDIR -- '$DAPORTALDIR' &&
+		cd '$DAPORTALDIR' &&
+		$TARX '$TMPDIR/$ARCHIVE' &&
+		$RM -- '$TMPDIR/$ARCHIVE' &&
+		$CP -r -- '$PACKAGE-$VERSION/AUTHORS' \
+			'$PACKAGE-$VERSION/BUGS' \
+			'$PACKAGE-$VERSION/COPYING' \
+			'$PACKAGE-$VERSION/INSTALL' \
+			'$PACKAGE-$VERSION/Makefile' \
+			'$PACKAGE-$VERSION/README.md' \
+			'$PACKAGE-$VERSION/config.sh' \
+			'$PACKAGE-$VERSION/data/' \
+			'$PACKAGE-$VERSION/doc/' \
+			'$PACKAGE-$VERSION/po/' \
+			'$PACKAGE-$VERSION/project.conf' \
+			'$PACKAGE-$VERSION/src/' \
+			'$PACKAGE-$VERSION/tests/' \
+			'$PACKAGE-$VERSION/tools/' . &&
+			$RM -r -- $PACKAGE-$VERSION\""		|| return 2
+	$DEBUG $RM -- "$ARCHIVE"
+}
+
+
+#debug
+_debug()
+{
+	[ "$VERBOSE" -ne 0 ] && echo "$@"
+	"$@"
+}
+
+
 #usage
 _usage()
 {
-	echo "Usage: deploy.sh [-P prefix] hostname" 1>&2
+	echo "Usage: deploy.sh [-P prefix][-v] hostname" 1>&2
+	echo "  -v    Verbose mode" 1>&2
 	return 1
 }
 
@@ -46,10 +89,14 @@ if [ -z "$PACKAGE" -o -z "$VERSION" ]; then
 	exit 2
 fi
 ARCHIVE="$PACKAGE-$VERSION.tar.gz"
-while getopts P: name; do
+while getopts "P:v" name; do
 	case "$name" in
 		P)
 			PREFIX="$OPTARG"
+			;;
+		v)
+			DEBUG="_debug"
+			VERBOSE=1
 			;;
 		?)
 			_usage
@@ -65,17 +112,4 @@ fi
 DAPORTALDIR="$PREFIX/daportal"
 REMOTE="$1"
 
-$MAKE dist &&
-$SCP -- "$ARCHIVE" "$REMOTE:$TMPDIR" &&
-$SSH "$REMOTE" "sh -c \"$MKDIR -- '$DAPORTALDIR' &&
-	cd '$DAPORTALDIR' &&
-	$TAR -xzf '$TMPDIR/$ARCHIVE' &&
-	$RM -- '$TMPDIR/$ARCHIVE' &&
-	$CP -r -- '$PACKAGE-$VERSION/AUTHORS' '$PACKAGE-$VERSION/BUGS' \
-		'$PACKAGE-$VERSION/COPYING' '$PACKAGE-$VERSION/INSTALL' \
-		'$PACKAGE-$VERSION/Makefile' '$PACKAGE-$VERSION/README.md' \
-		'$PACKAGE-$VERSION/config.sh' '$PACKAGE-$VERSION/data/' \
-		'$PACKAGE-$VERSION/doc/' '$PACKAGE-$VERSION/po/' \
-		'$PACKAGE-$VERSION/project.conf' '$PACKAGE-$VERSION/src/' \
-		'$PACKAGE-$VERSION/tests/' '$PACKAGE-$VERSION/tools/' . &&
-	$RM -r -- $PACKAGE-$VERSION\""
+_deploy "$DAPORTALDIR" "$REMOTE"
