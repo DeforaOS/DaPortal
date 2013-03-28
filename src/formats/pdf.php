eff8dacf6afc34791254d6ca27a012e19c33542e
Pierre Pronchery <khorben@defora.org>
2013-03-28 18:35:09 +0100

    Beginning to output anything at all (untested)

--- src/formats/pdf.php
+++ src/formats/pdf.php
@@ -1,5 +1,5 @@
 <?php //$Id$
-//Copyright (c) 2012 Pierre Pronchery <khorben@defora.org>
+//Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org>
 //This file is part of DeforaOS Web DaPortal
 //
 //This program is free software: you can redistribute it and/or modify
@@ -19,12 +19,12 @@
 require_once('./system/format.php');
 
 
-//PdfFormat
-class PdfFormat extends Format
+//PDFFormat
+class PDFFormat extends FormatElements
 {
 	//methods
 	//essential
-	//PdfFormat::match
+	//PDFFormat::match
 	protected function match($engine, $type = FALSE)
 	{
 		switch($type)
@@ -37,7 +37,7 @@ class PdfFormat extends Format
 	}
 
 
-	//PdfFormat::attach
+	//PDFFormat::attach
 	protected function attach($engine, $type = FALSE)
 	{
 	}
@@ -46,11 +46,224 @@ class PdfFormat extends Format
 	//public
 	//methods
 	//rendering
-	//PdfFormat::render
+	//PDFFormat::render
 	public function render($engine, $page, $filename = FALSE)
 	{
-		//FIXME implement
+		$user = $engine->getCredentials();
+		//XXX obtain the full name instead
+		$author = $user->getUsername();
+		$title = $page->getProperty('title');
+
+		$this->pdf = PDF_new();
+		PDF_set_info($this->pdf, "Creator", "DaPortal");
+		PDF_set_info($this->pdf, "Author", $author);
+		if($title !== FALSE)
+			PDF_set_info($this->pdf, "Title", $title);
+		print(PDF_get_buffer($this->pdf));
+		PDF_delete($this->pdf);
 	}
+
+
+	//protected
+	//methods
+	//rendering
+	//PDFFormat::renderBlock
+	protected function renderBlock($e)
+	{
+		if(($text = $e->getProperty('text')) === FALSE)
+			$text = '';
+		PDF_continue_text($this->pdf, $text);
+	}
+
+
+	//PDFFormat::renderButton
+	protected function renderButton($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderCheckbox
+	protected function renderCheckbox($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderCombobox
+	protected function renderCombobox($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderDialog
+	protected function renderDialog($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderEntry
+	protected function renderEntry($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderFileChooser
+	protected function renderFileChooser($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderForm
+	protected function renderForm($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderFrame
+	protected function renderFrame($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderHbox
+	protected function renderHbox($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderHtmledit
+	protected function renderHtmledit($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderHtmlview
+	protected function renderHtmlview($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderIconview
+	protected function renderIconview($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderImage
+	protected function renderImage($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderInline
+	protected function renderInline($e)
+	{
+		$text = $e->getProperty('text');
+
+		if($text !== FALSE)
+			PDF_show($this->pdf, $text);
+	}
+
+
+	//PDFFormat::renderLabel
+	protected function renderLabel($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderLink
+	protected function renderLink($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderMenubar
+	protected function renderMenubar($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderPage
+	protected function renderPage($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderProgress
+	protected function renderProgress($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderRadioButton
+	protected function renderRadioButton($e)
+	{
+		$this->renderInline($e);
+	}
+
+
+	//PDFFormat::renderStatusbar
+	protected function renderStatusbar($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderTextview
+	protected function renderTextview($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderTitle
+	protected function renderTitle($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderToolbar
+	protected function renderToolbar($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderTreeview
+	protected function renderTreeview($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//PDFFormat::renderVbox
+	protected function renderVbox($e)
+	{
+		$this->renderBlock($e);
+	}
+
+
+	//private
+	//properties
+	private $pdf;
 }
 
 ?>
