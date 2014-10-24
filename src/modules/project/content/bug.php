12f43638bebb6ce2c64c339ed931bf3cdf17cbb9
Pierre Pronchery <khorben@defora.org>
2014-10-24 19:02:33 +0200

    Fixed listing bug reports

--- src/modules/project/content/bug.php
+++ src/modules/project/content/bug.php
@@ -258,25 +258,46 @@ class BugProjectContent extends ContentMulti
 		daportal_content_public project, daportal_project
 		WHERE bug.content_id=daportal_bug.content_id
 		AND daportal_bug.project_id=daportal_project.project_id
-		AND project.content_id=daportal_project.project_id";
+		AND project.content_id=daportal_project.project_id
+		AND bug.module_id=:module_id";
 	//IN:	module_id
 	//	user_id
-	static protected $bug_query_list_user = "SELECT bug_id,
-		bug.content_id AS id, bug.timestamp AS timestamp,
-		daportal_user_enabled.user_id AS user_id, username,
-		bug.title AS title, bug.enabled AS enabled, state, type,
-		priority, daportal_project.project_id AS project_id,
-		project.title AS project, daportal_bug.public AS public
-		FROM daportal_content_public bug, daportal_module,
-		daportal_user_enabled, daportal_bug,
+	static protected $query_list_user = "SELECT bug.content_id AS id,
+		bug.timestamp AS timestamp,
+		bug.module_id AS module_id, bug.module AS module,
+		bug.user_id AS user_id, bug.username AS username,
+		bug.group_id AS group_id, bug.groupname AS groupname,
+		bug.title AS title, bug.content AS content,
+		bug.enabled AS enabled, bug.public AS public,
+		bug_id, state, type, priority,
+		daportal_project.project_id AS project_id,
+		project.title AS project
+		FROM daportal_content_public bug, daportal_bug,
 		daportal_content_public project, daportal_project
-		WHERE bug.module_id=daportal_module.module_id
-		AND daportal_module.module_id=:module_id
-		AND bug.user_id=daportal_user_enabled.user_id
-		AND bug.content_id=daportal_bug.content_id
+		WHERE bug.content_id=daportal_bug.content_id
 		AND daportal_bug.project_id=daportal_project.project_id
 		AND project.content_id=daportal_project.project_id
-		AND daportal_content_public.user_id=:user_id";
+		AND bug.module_id=:module_id
+		AND bug.user_id=:user_id";
+	//IN:	module_id
+	//	user_id
+	static protected $query_list_user_private = "SELECT bug.content_id AS id,
+		bug.timestamp AS timestamp,
+		bug.module_id AS module_id, bug.module AS module,
+		bug.user_id AS user_id, bug.username AS username,
+		bug.group_id AS group_id, bug.groupname AS groupname,
+		bug.title AS title, bug.content AS content,
+		bug.enabled AS enabled, bug.public AS public,
+		bug_id, state, type, priority,
+		daportal_project.project_id AS project_id,
+		project.title AS project
+		FROM daportal_content_enabled bug, daportal_bug,
+		daportal_content_public project, daportal_project
+		WHERE bug.content_id=daportal_bug.content_id
+		AND daportal_bug.project_id=daportal_project.project_id
+		AND project.content_id=daportal_project.project_id
+		AND bug.module_id=:module_id
+		AND bug.user_id=:user_id";
 	//IN:	module_id
 	//	user_id
 	//	content_id
