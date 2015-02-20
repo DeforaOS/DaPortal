c8ca93c3552a0b67bf5c1226aa87248e4782e82d
Pierre Pronchery <khorben@defora.org>
2015-02-20 18:48:55 +0100

    Redirect to the original page when logging in through the login widget

--- src/modules/user/module.php
+++ src/modules/user/module.php
@@ -1561,7 +1561,13 @@ class UserModule extends Module
 	{
 		$cred = $engine->getCredentials();
 
-		$request = $engine->getRequest();
+		$r = $engine->getRequest();
+		//FIXME also keep the original parameters
+		$r = $this->getRequest(FALSE, array(
+			'username' => $request->get('username'),
+			'module' => $r->getModule(),
+			'action' => $r->getAction(),
+			'id' => $r->getID(), 'title' => $r->getTitle()));
 		if($cred->getUserID() == 0)
 			return $this->formLogin($engine, $request, FALSE);
 		$box = new PageElement('vbox');
