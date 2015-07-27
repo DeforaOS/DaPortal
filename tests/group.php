2194e861b7ed82d96376cc8c64a66baa6df6bdc3
Pierre Pronchery <khorben@defora.org>
2015-07-27 18:49:22 +0200

    Add a "group" test

---
+++ tests/group.php
@@ -0,0 +1,31 @@
+<?php //$Id$
+//Copyright (c) 2015 Pierre Pronchery <khorben@defora.org>
+//This file is part of DeforaOS Web DaPortal
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 3 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+require_once('./tests.php');
+
+
+//functions
+if(($group = Group::lookup($engine, 'nogroup')) === FALSE
+		|| $group->getGroupID() != 0)
+	exit(2);
+if($group->setEnabled($engine, FALSE) === FALSE
+		|| $group->setEnabled($engine, TRUE) === FALSE)
+	exit(3);
+exit(0);
+
+?>
