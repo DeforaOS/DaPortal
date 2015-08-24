3a50563b94db42042dbae6f30d32e356e3ba0bfb
Pierre Pronchery <khorben@defora.org>
2015-08-24 17:59:24 +0200

    Code cleanup

--- src/modules/download/content/folder.php
+++ src/modules/download/content/folder.php
@@ -235,7 +235,7 @@ class FolderDownloadContent extends DownloadContent
 			return FALSE;
 		}
 		if(($did = $db->getLastID($engine, static::$download_table,
-				static::$download_id)) === FALSE)
+				static::$download_table_id)) === FALSE)
 			return FALSE;
 		//reflect the new properties
 		$this->set('download_id', $did);
