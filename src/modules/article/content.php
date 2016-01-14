74bed0a04ee763c23e14d9234158f9d3947dc41d
Pierre Pronchery <khorben@defora.org>
2016-01-14 19:51:56 +0100

    Update the copyright notice

--- src/modules/article/content.php
+++ src/modules/article/content.php
@@ -1,5 +1,5 @@
 <?php //$Id$
-//Copyright (c) 2013-2014 Pierre Pronchery <khorben@defora.org>
+//Copyright (c) 2013-2016 Pierre Pronchery <khorben@defora.org>
 //This file is part of DeforaOS Web DaPortal
 //
 //This program is free software: you can redistribute it and/or modify
