4a2c452cbacfaebc87a261edb4b664d25ce46790
Pierre Pronchery <khorben@defora.org>
2016-01-19 17:06:19 +0100

    Let DatabaseTransaction be observable

--- src/system/database/transaction.php
+++ src/system/database/transaction.php
@@ -17,15 +17,24 @@
 
 
 //DatabaseTransaction
-class DatabaseTransaction
+class DatabaseTransaction implements Observable
 {
 	//public
+	//properties
+	static public $STATUS_INITIAL = 0;
+	static public $STATUS_COMMIT_PENDING = 1;
+	static public $STATUS_ROLLBACK_PENDING = 2;
+	static public $STATUS_COMMIT = 3;
+	static public $STATUS_ROLLBACK = 4;
+
+
 	//methods
 	//essential
 	//DatabaseTransaction::DatabaseTransaction
 	public function __construct(Database $database)
 	{
 		$this->database = $database;
+		$this->observers = new \SplObjectStorage();
 	}
 
 
@@ -38,10 +47,56 @@ class DatabaseTransaction
 
 
 	//accessors
+	//DatabaseTransaction::getStatus
+	public function getStatus()
+	{
+		if($this->count < 0)
+			return static::$STATUS_INITIAL;
+		if($this->count >= 1)
+			return $this->rollback
+				? static::$STATUS_ROLLBACK_PENDING
+				: static::$STATUS_COMMIT_PENDING;
+		return $this->rollback
+			? static::$STATUS_ROLLBACK
+			: static::$STATUS_COMMIT;
+	}
+
+
 	//DatabaseTransaction::inTransaction
 	public function inTransaction()
 	{
 		return $this->count > 0;
+	}
+
+
+	//Observable
+	//DatabaseTransaction::addObserver
+	public function addObserver(Observer $observer)
+	{
+		if($this->observers->contains($observer))
+			return FALSE;
+		$this->observers->attach($observer);
+		return TRUE;
+	}
+
+
+	//DatabaseTransaction::notifyObservers
+	public function notifyObservers()
+	{
+		//work on a copy in case Observers remove themselves
+		$observers = clone $this->observers;
+		foreach($observers as $observer)
+			$observer->notify($this);
+	}
+
+
+	//DatabaseTransaction::removeObserver
+	public function removeObserver(Observer $observer)
+	{
+		if(!$this->observers->contains($observer))
+			return FALSE;
+		$this->observers->detach($observer);
+		return TRUE;
 	}
 
 
@@ -85,6 +140,7 @@ class DatabaseTransaction
 			$res = $this->databaseRollback();
 		else if(($res = $this->databaseCommit()) === FALSE)
 			$this->rollback = TRUE;
+		$this->notifyObservers();
 		$this->database->transactionComplete();
 		return $res;
 	}
@@ -146,6 +202,9 @@ class DatabaseTransaction
 
 	//private
 	//properties
+	//Observable
+	private $observers;
+
 	private $database;
 	private $count = -1;
 	private $rollback = FALSE;
