007144cabd1e3e15c4583ee54b4f6e324b5dbe2e
Pierre Pronchery <khorben@defora.org>
2016-02-17 02:33:25 +0100

    Give hints about the size of stock icons by default

--- src/formats/html.php
+++ src/formats/html.php
@@ -840,8 +840,8 @@ class HTMLFormat extends FormatElements
 				//FIXME use the actual icon
 				$attributes['src'] = 'data:image/png'
 					.';base64,'.base64_encode($img);
-			$attributes['width'] = 0;
-			$attributes['height'] = 0;
+			$attributes['width'] = $size;
+			$attributes['height'] = $size;
 		}
 		else if(($r = $e->get('request')) !== FALSE)
 			$attributes['src'] = $this->engine->getURL($r, FALSE);
