3d60dab2a775de56b775d5135f62548f14742485
Pierre Pronchery <khorben@defora.org>
2016-03-10 18:13:34 +0100

    Better support for friendly URLs

--- data/daportal.php
+++ data/daportal.php
@@ -1,1 +1,1 @@
-<?php $_SERVER['SCRIPT_NAME'] = '/index.php'; require('./index.php'); ?>
+<?php $_SERVER['SCRIPT_NAME'] = dirname($_SERVER['SCRIPT_NAME']).'/index.php'; require('./index.php'); ?>
--- data/icons/generic/16x16/index.php
+++ data/icons/generic/16x16/index.php
@@ -1,1 +1,1 @@
-<?php require('../index.php'); ?>
+<?php $_SERVER['SCRIPT_NAME'] = dirname(dirname($_SERVER['SCRIPT_NAME'])).'/index.php'; require('../index.php'); ?>
