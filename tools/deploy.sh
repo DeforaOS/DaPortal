49f44932275b877e7a536d8285b159be1da774b3
Pierre Pronchery <khorben@defora.org>
2016-04-14 16:49:04 +0200

    Update the copyright notice

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2012-2014 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2012-2016 Pierre Pronchery <khorben@defora.org>
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, version 3 of the License.
